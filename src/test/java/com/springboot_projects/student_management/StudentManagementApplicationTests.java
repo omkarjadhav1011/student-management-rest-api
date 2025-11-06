e253dee6b34bd531e9c174386d2a8acc9955ff6c
Omkar Jadhav <jadhavomkar101103@gmail.com>
2025-11-06 18:19:54 +0530

    Initial commit

---
+++ src/test/java/com/springboot_projects/student_management/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_projects.student_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
